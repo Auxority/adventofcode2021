103d410dccfa36852d41998fe4ac678b3967104d
Auxority <gide0013@hz.nl>
2021-12-06 15:28:40 +0100

    improved day 6

--- src/day6.ts
+++ src/day6.ts
@@ -1,24 +1,40 @@
 import AdventOfCode from "./AdventOfCode.js";
 
-function simulateDay(lanternFish: number[]): void {
-    for (let i = 0; i < lanternFish.length; i++) {
-        if (lanternFish[i] === 0) {
-            lanternFish[i] = 7;
-            lanternFish.push(9);
-        }
-        lanternFish[i]--;
+function getInternalTimers(lanternFish: number[]): number[] {
+    const timers: number[] = new Array(9).fill(0);
+    lanternFish.forEach((internalTimer) => timers[internalTimer]++);
+    return timers;
+}
+
+function simulateDay(internalTimers: number[]): number[] {
+    const newTimers: number[] = new Array(9).fill(0).map((_, index) => internalTimers[(index + 1) % internalTimers.length]);
+    newTimers[6] += internalTimers[0];
+    return newTimers;
+}
+
+function countTotalFish(internalTimers: number[]): number {
+    return internalTimers.reduce((total, current) => total + current, 0);
+}
+
+function part1(internalTimers: number[]) {
+    for (let i = 0; i < 80; i++) {
+        internalTimers = simulateDay(internalTimers);
     }
+    console.log(countTotalFish(internalTimers));
+}
+
+function part2(internalTimers: number[]) {
+    for (let i = 0; i < 256; i++) {
+        internalTimers = simulateDay(internalTimers);
+    }
+    console.log(countTotalFish(internalTimers));
 }
 
 (async() => {
     const input: string = await AdventOfCode.getRawInput(6, 2021);
-
     const lanternFish: number[] = input.split(",").map(Number);
-    for (let i = 0; i < 80; i++) {
-        simulateDay(lanternFish);
-    }
-    console.log(lanternFish.length);
-    // read & write streams
-    // Maybe it's an idea to write to the end of a file
-    // And read the file line by line (maybe 1,000,000 fish per line)
+
+    const internalTimers: number[] = getInternalTimers(lanternFish);
+    part1(internalTimers);
+    part2(internalTimers);
 })();
